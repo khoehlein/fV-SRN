80e03e36bf314472c637f7c54ca19ad73ca8613c
khoehlein <kevin.hoehlein@tum.de>
2022-06-07 15:16:19 +0200

    pyrenderer running on server

--- compile-library-server.sh
+++ compile-library-server.sh
@@ -2,11 +2,13 @@
 echo CMake
 mkdir build
 cd build
-cmake -DTORCH_PATH=~/anaconda3/envs/py38torch18/lib/python3.8/site-packages/torch -DRENDERER_BUILD_GUI=OFF -DRENDERER_BUILD_TESTS=OFF -DRENDERER_BUILD_CLI=OFF -DRENDERER_BUILD_TESTS=OFF -DRENDERER_BUILD_OPENGL_SUPPORT=OFF ..
+cmake -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-11.1 -DCMAKE_CUDA_COMPILER=/usr/local/cuda-11.1/bin/nvcc -DTORCH_PATH=~/anaconda3/envs/fvsrn2/lib/python3.8/site-packages/torch -DRENDERER_BUILD_GUI=OFF -DRENDERER_BUILD_TESTS=OFF -DRENDERER_BUILD_CLI=OFF -DRENDERER_BUILD_TESTS=OFF -DRENDERER_BUILD_OPENGL_SUPPORT=OFF ..
+#cmake -DTORCH_PATH=~/anaconda3/envs/py38torch18/lib/python3.8/site-packages/torch -DRENDERER_BUILD_GUI=OFF -DRENDERER_BUILD_TESTS=OFF -DRENDERER_BUILD_CLI=OFF -DRENDERER_BUILD_TESTS=OFF -DRENDERER_BUILD_OPENGL_SUPPORT=OFF ..
 make -j8 VERBOSE=true
 cd ..
 
 echo Setup-Tools build
+export PATH=/usr/local/cuda-11.1/bin:$PATH
 python setup.py build
 cp build/lib.linux-x86_64-3.8/pyrenderer.cpython-38-x86_64-linux-gnu.so bin/
 
